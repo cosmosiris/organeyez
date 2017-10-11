7237e8284c59ec3dc124a88672965d46dbeb1757
Iris Nevins <ikn02010@gmail.com>
2017-10-11 10:45:05 -0700

    add pricing erb in views and pricing route in controller

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-get '/' do
-  erb :'static/home'
-end
-
-get '/setup' do
-  erb :'static/setup'
-end
-
-
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+get '/' do
+  erb :'static/home'
+end
+
+get '/setup' do
+  erb :'static/setup'
+end
+
+get '/pricing' do
+  erb :'static/pricing'
+end
+
+get '/founder' do
+  erb :'static/founder'
+end
+
