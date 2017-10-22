25cdc933a8086cdbe4ee3451505512575b81805a
Iris Nevins <ikn02010@gmail.com>
2017-10-22 13:18:31 -0700

    add initial edit functionality for name, start date, and end date

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,43 @@
-get '/org_sessions/projects/new' do
+post '/projects' do
+  @project = current_org.projects.new(params[:project])
+  if @project.save
+    if request.xhr?
+      status 200
+      erb :"portal/projects/_projects", layout: false, locals: { project: @project}
+    else
+      redirect "/org_sessions/#{current_org.id}"
+    end
+  else
+    @errors = @project.errors.full_messages
+    if request.xhr?
+      status 422
+      erb :_errors, layout: false, locals: {errors: @errors }
+    else
+      redirect "/org_sessions/#{current_org.id}"
+    end
+  end
+end
+
+put '/projects' do
+  @project = Project.find(params[:project_id])
+  @project[:name] = params[:name]
+  @project[:start_date] = params[:start_date]
+  @project[:end_date] = params[:end_date]
+  if @project.save
+    redirect "/projects/#{@project.id}/edit"
+  else
+    @errors = ["Your message did not save"]
+    erb :_errors, layout: false, locals: {errors: @errors }
+  end
+end
+
+delete '/projects/:id' do
+    p "************************"
+    Project.find(params[:id]).destroy
+    redirect "/org_sessions/#{current_org.id}"
+end
+
+get '/org_sessions/:id/projects/new' do
   if request.xhr?
     erb :'portal/projects/new', layout: false
   else
@@ -6,7 +45,8 @@ get '/org_sessions/projects/new' do
   end
 end
 
-post '/projects' do
-  current_org.projects.create(params[:project])
-  redirect "/org_sessions/#{current_org.id}"
+get '/projects/:id/edit' do
+  @project = Project.find(params[:id])
+  erb :'portal/projects/edit', :layout => :'layouts/organization_layout'
 end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,63 @@
 $(document).ready(function() {
   renderNewProjectForm()
+  deleteProject()
+  submitNewProject()
 });
 
 var renderNewProjectForm = function(){
-  $("#new_project_button").on("submit", function(event){
+  $("#new_project_button").on("click", function(event){
+    event.preventDefault();
+
+    $("#new_project_button").hide();
+    $("#new_project_form").removeClass("hidden");
+  });
+};
+
+var submitNewProject = function(){
+  $("#new_project_container").on("submit", "#new_project_form", function(event){
     event.preventDefault();
 
     var url = $(this).attr("action")
     var method = $(this).attr("method")
+    data = $(this).serialize();
+    console.log(this)
 
     $.ajax({
       url: url,
-      method: method
+      method: method,
+      data: data
     }).done(function(response){
-      $("#new_project_button").after(response);
+      $("#new_project_form").addClass("hidden");
+      $("#project_display_list").append(response);
+    }).fail(function(error){
+       $("#project_display_list").append(error.responseText);
+    });
+  $("#new_project_button").show();
+  });
+}
+
+
+
+
+var deleteProject = function(){
+   $("#project_display_list").on("submit", ".delete_project_form", function(event){
+    event.preventDefault();
+    var url = $(this).attr("action")
+    var method = $(this).attr("method")
+    var data = $(this).serialize();
+    console.log("heyyyyyyyyy");
+
+    $.ajax({
+      url: url,
+      method: method,
+      context: this,
+      data: data
+    }).done(function(response){
+      $(this).closest(".homepage_project_view").remove();
+      console.log("removed");
+    }).fail(function(error){
+      $("#new_project_button").prepend(error.responseText);
     });
   });
 };
-
 
