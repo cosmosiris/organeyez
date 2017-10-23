2acbf34ea611a44b728260247d7aa8110c711fe4
Iris Nevins <ikn02010@gmail.com>
2017-10-23 14:32:04 -0700

    add name check to organization_spec

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe Organization do
   # pending "add some examples to (or delete) /Users/apprentice/iris/organeyez/Rakefile"
-  let(:dd) {Organization.find_by(email: "dd@gmail.com")}
+  let(:dd) {Organization.create(name: "Dream Defenders", email: "dd@gmail.com", password: "1234")}
 
 
     it 'has a name' do
