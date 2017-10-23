509a82a271cb2dd594dd32637a1b29b36775c1ac
Iris Nevins <ikn02010@gmail.com>
2017-10-23 14:44:30 -0700

    re-add dd variable to seed file

--- db/migrate/20171008171506_create_tasks.rb
+++ db/migrate/20171008171506_create_tasks.rb
@@ -2,9 +2,10 @@ class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
       t.string   :name, { null: false, limit: 50 }
+      t.string   :description
+      t.text     :notes
       t.references  :project, { null: false }
       t.references  :leader, { null: false }
-      t.string   :message, { null: false }
 
       t.timestamps
     end
--- db/migrate/20171014214649_add_mission_to_organizations.rb
+++
@@ -1,6 +0,0 @@
-class AddMissionToOrganizations < ActiveRecord::Migration[5.1]
-  def change
-    add_column :organizations, :mission, :text, default:
-    ""
-  end
-end
