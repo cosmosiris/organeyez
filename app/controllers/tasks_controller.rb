0ed75cc5e47574d6727b1e93789ac44c8645ad2d
Iris Nevins <ikn02010@gmail.com>
2017-10-24 11:17:10 -0700

    add ajax to task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,26 @@ post '/tasks' do
   @project = Project.find(params[:project_id])
   @task = @project.tasks.new(params[:task])
   if @task.save
-    redirect "/projects/#{@project.id}/edit"
+    if request.xhr?
+      status 200
+      erb :"portal/tasks/_tasks", layout: false, locals: { task: @task}
+    else
+      redirect "/projects/#{@project.id}/edit"
+    end
   else
     @errors = @task.errors.full_messages
-    erb :_errors, layout: false, locals: {errors: @errors }
+    if request.xhr?
+      status 422
+      erb :_errors, layout: false, locals: {errors: @errors }
+    else
+      redirect "/projects/#{@project.id}/edit"
+    end
   end
 end
 
 get "/org_sessions/:id/projects/:id/edit/tasks/new" do
    if request.xhr?
-    erb :'portal/tasks/new', layout: false
+    redirect ''
   else
     erb :'portal/tasks/new'
   end
@@ -20,6 +30,11 @@ end
 delete '/tasks/:id' do
   Task.find_by(id: params[:id]).destroy
   @project = Project.find(params[:project_id])
-  redirect "/projects/#{@project.id}/edit"
+  if request.xhr?
+    status 200
+    redirect "/projects/#{@project.id}/edit"
+  else
+    redirect "/projects/#{@project.id}/edit"
+  end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  renderNewProjectForm()
-  deleteProject()
-  submitNewProject()
-  renderNewTaskForm()
+  renderNewProjectForm();
+  deleteProject();
+  submitNewProject();
+  renderNewTaskForm();
+  submitNewTask();
+  deleteTask();
 });
 
 var renderNewProjectForm = function(){
   $("#new_project_button").on("click", function(event){
     event.preventDefault();
-
+    console.log("success")
     $("#new_project_button").hide();
     $("#new_project_form").removeClass("hidden");
   });
@@ -18,6 +20,7 @@ var submitNewProject = function(){
   $("#new_project_container").on("submit", "#new_project_form", function(event){
     event.preventDefault();
 
+    form = this
     var url = $(this).attr("action")
     var method = $(this).attr("method")
     data = $(this).serialize();
@@ -27,13 +30,14 @@ var submitNewProject = function(){
       method: method,
       data: data
     }).done(function(response){
-      $("#new_project_form").addClass("hidden");
       $("#project_display_list").append(response);
-      console.log(this)
+      form.reset();
+      console.log("success")
     }).fail(function(error){
-       $("#project_display_list").append(error.responseText);
+       $("#project_display_list").prepend(error.responseText);
     });
-  $("#new_project_button").show();
+    $("#new_project_form").addClass("hidden");
+    $("#new_project_button").show();
   });
 }
 
@@ -44,7 +48,7 @@ var deleteProject = function(){
     var url = $(this).attr("action")
     var method = $(this).attr("method")
     var data = $(this).serialize();
-    console.log("heyyyyyyyyy");
+    console.log("success");
 
     $.ajax({
       url: url,
@@ -52,10 +56,10 @@ var deleteProject = function(){
       context: this,
       data: data
     }).done(function(response){
-      $(this).closest(".homepage_project_view").remove();
+      $(this).closest(".homepage_project_view_box").remove();
       console.log("removed");
     }).fail(function(error){
-      $("#new_project_button").prepend(error.responseText);
+      $(".homepage_project_view_box").prepend(error.responseText);
     });
   });
 };
@@ -63,8 +67,54 @@ var deleteProject = function(){
 var renderNewTaskForm = function(){
   $("#new_task_button").on("click", function(event){
     event.preventDefault();
-
+    console.log("SUCCESS");
     $("#new_task_button").hide();
     $("#new_task_form").removeClass("hidden");
+  });
+};
+
+var submitNewTask = function(){
+  $("#new_task_form").on("submit", function(event){
+    event.preventDefault();
+
+    url = $(this).attr("action");
+    method = $(this).attr("method");
+    data = $(this).serialize();
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    }).done(function(response){
+      $("#project_task_list").append(response);
+      console.log("SUCCESS");
+    }).fail(function(error){
+      $("#project_task_list").prepend(error.responseText);
+      console.log("fail");
+    });
+      $("#new_task_button").show();
+      $("#new_task_form").addClass("hidden");
+  });
+}
+
+var deleteTask = function(){
+   $("#project_display_list").on("submit", ".delete_project_form", function(event){
+    event.preventDefault();
+    var url = $(this).attr("action")
+    var method = $(this).attr("method")
+    var data = $(this).serialize();
+    console.log("success");
+
+    $.ajax({
+      url: url,
+      method: method,
+      context: this,
+      data: data
+    }).done(function(response){
+      $(this).closest(".homepage_project_view_box").remove();
+      console.log("removed");
+    }).fail(function(error){
+      $(".homepage_project_view_box").prepend(error.responseText);
+    });
   });
 };
