2cd68b6fc4987c383c65851bb0061ec050c10de6
Iris Nevins <ikn02010@gmail.com>
2017-10-23 20:28:34 -0700

    add css to tasks and create task deletion functionality

--- db/migrate/20171008171506_create_tasks.rb
+++ db/migrate/20171008171506_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string   :name, { null: false, limit: 50 }
-      t.string   :description
-      t.text     :notes
-      t.references  :project, { null: false }
-      t.references  :leader, { null: false }
+      t.string   :name, limit: 50, default: ""
+      t.string   :description, default: ""
+      t.text     :notes, default: ""
+      t.references  :project
+      t.references  :leader
 
       t.timestamps
     end
